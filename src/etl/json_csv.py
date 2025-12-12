5d5c5a2bd28e642d7f24a3a767ca3f894f83d0fa
Premanshu Bhagat <premtsapj@gmail.com>
2025-12-12 01:58:34 -0500

    added schema validation.

--- src/etl/json_csv.py
+++ src/etl/json_csv.py
@@ -5,10 +5,21 @@ import os
 import json
 import argparse
 import pandas as pd
+from jsonschema import validate, ValidationError
 
-def json_csv(json_path, csv_path):
+def json_csv(json_path, csv_path, schema_path=None):
     with open(json_path, "r", encoding="utf-8") as f:
         data = json.load(f)
+
+    if schema_path:
+        with open(schema_path, "r", encoding="UTF-8") as f:
+            schema = json.load(f)
+        try:
+            validate(instance=data, schema=schema)
+            print("==> JSON Validated.")
+        except:
+            print("==> JSON Validation Failed.")
+            return
 
     if isinstance(data, dict) and len(data) == 1:
         key = next(iter(data))
@@ -26,6 +37,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Extract and transform JSON to CSV")
     parser.add_argument("--i", required=True, help="Inp JSON")
     parser.add_argument("--o", required=False, help="Out CSV")
+    parser.add_argument("--schema", required=False, help="Optional JSON schema for validation")
 
     args = parser.parse_args()
 
@@ -33,5 +45,5 @@ if __name__ == "__main__":
         base = os.path.splitext(os.path.basename(args.i))[0]
         args.o = f"../../data/processed/{base}.csv"
     
-    json_csv(args.i, args.o)
+    json_csv(args.i, args.o, schema_path=args.schema)
 
